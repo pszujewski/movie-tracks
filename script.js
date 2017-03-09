bdeb062104fc210eda69dd9963c1ca34d81485e5
Peter Szujewski <pszujewski@gmail.com>
2017-03-09 18:17:16 -0500

    connected

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
+// For querying the MovieDB for a movie title's ID
+const S_BASE_URL="https://api.themoviedb.org/3/search/movie?api_key=1710c94a1d9a1c75e363bf47a0f446b3";
+// The url path for getting the poster image in the Movie DB
+const MOVIE_API_KEY = "?api_key=1710c94a1d9a1c75e363bf47a0f446b3";
+
 // Object for managing state
 var state = {
   movieData: {},
-  musicData: {}
+  musicData: {},
+  rndmFilms: []
 };
 
-
-// for searching the DB for a movie title's ID
-const S_BASE_URL="https://api.themoviedb.org/3/search/movie?api_key=1710c94a1d9a1c75e363bf47a0f446b3";
-// The url path for getting the poster image
-const MEDIA_PATH = "https://image.tmdb.org/t/p/w500";
-const API_KEY = "?api_key=1710c94a1d9a1c75e363bf47a0f446b3";
-
+// Empty arrays to hold separate data from the movie DB. JQuery UI's autocomplete
+// method will point to movieTitles as its 'source'
+// These arrays will be populated with data from the Movie DB upon loading the doc
 var genreIds = [];
 var movieTitles = [];
 
+//To generate random numbers
+function doRndm() {
+  return Math.floor(Math.random()*211);
+}
+
+// Functions for populating the movies titles array for the autocomplete functionality
+// query to movie DM to get a list of titles under each genre
 function getTitles(id) {
-  var genreReq = `https://api.themoviedb.org/3/genre/${id}/movies${API_KEY}&language=en-US&include_adult=false&sort_by=created_at.asc`;
+  let genreReq = `https://api.themoviedb.org/3/genre/${id}/movies${MOVIE_API_KEY}&language=en-US&include_adult=false&sort_by=created_at.asc`;
   $.ajax({
     url: genreReq,
     success: function(data) {
@@ -28,9 +37,12 @@ function getTitles(id) {
   });
 }
 
-function getGenres() {
-  // for getting the list of genres and their ids
-  var listRequest = `https://api.themoviedb.org/3/genre/movie/list${API_KEY}&language=en-US`;
+// Query the movie DM to get a list of all genres with their ids.
+// Save the ids in the genreIds array and invoke getTitles for each genre id
+function getGenres(state) {
+  let listRequest = `https://api.themoviedb.org/3/genre/movie/list${MOVIE_API_KEY}&language=en-US`;
+  let rndm = [doRndm(), doRndm(), doRndm()];
+  console.log(rndm);
   $.ajax({
     url: listRequest,
     success: function(result) {
@@ -40,100 +52,84 @@ function getGenres() {
       genreIds.forEach(function(id) {
         getTitles(id);
       });
-      //console.log(movieTitles);
+      rndm.forEach(function(num) {
+        state.rndmFilms.push(movieTitles[num]);
+      });
+      console.log(state.rndmFilms);
     }
   });
 }
 
-// API calls and handle data for a user search
+// Funciton to handle API calls and handle data for a user search
 // Get the data from the movie API
-
-function getMovieData(userSearch) {....
-$.getJSON('http://moviedata.com/', function() {
-  // updateState() -->
-  // Poster image Link
-  // Description
-  // Any ratings
-  // year of release
-  // Cast
-  getMusicData(userSearch);
-});
-}
-
-// Get Data from the music API
-function getMusicData(userSearch) {....
-  $.getJSON('http://spotifydata.com/', function() {
-    // updateState() -->
-    // Album and tracks inside the album(s)
-    // Links to the audio file
-    // image of the album art
-    // composer or various artists
-    renderToDOM();
+function getMovieData(searchTerm) {
+  $.ajax({
+    url: S_BASE_URL,
+    data: {query: searchTerm},
+    success: function(data) {
+      let urlSearchID = "https://api.themoviedb.org/3/movie/"+data.results[0].id+MOVIE_API_KEY;
+      $.ajax({
+        url: urlSearchID,
+        success: function(result) {
+          state.movieData.title = result.title;
+          state.movieData.year = result.release_date.substr(0, 4);
+          state.movieData.poster = "https://image.tmdb.org/t/p/w500"+result.poster_path;
+          state.movieData.desc = result.overview;
+          state.movieData.rating = result.vote_average;
+          state.movieData.tagline = result.tagline;
+          state.movieData.site = result.homepage;
+          state.movieData.backdropImg = "https://image.tmdb.org/t/p/w500"+result.backdrop_path;
+          console.log(state.movieData);
+        }
+      });
+    }
   });
 }
 
-function loadData(userSearch) {
-  getMovieData(userSearch);
+// Get Data from the music API
+// function getMusicData(userSearch) {....
+//   $.getJSON('http://spotifydata.com/', function() {
+//     // updateState() -->
+//     // Album and tracks inside the album(s)
+//     // Links to the audio file
+//     // image of the album art
+//     // composer or various artists
+//     renderToDOM();
+//   });
+// }
+//
+// function loadData(userSearch) {
+//   getMovieData(userSearch);
+// }
+
+// Functions for rendering state to the DOM
+function renderData(state) {
+  // to do
 }
 
-// Event Listener
+// Event Listeners
+// Handle each time a user searches for a movie title
 function handleSearch($btn, $input) {
   $btn.on("click", function(e) {
     let movieLoaded = false;
     let spotifyLoaded = false;
     let userSearch = $input.val();
-    // loadData(function() { updateState() }
-    // renderToDOM();
   });
 }
 
-
-// Invoke Functions
-$(document).ready(function() {
-
-  getGenres();
-
-  $("#search").autocomplete({
+// Handle the autocomplete functionality
+function doAutocomplete($input) {
+  $input.autocomplete({
      source: movieTitles,
      minLength: 2,
      delay: 500
   });
+}
 
+// Invoke Functions, document ready...
+$(document).ready(function() {
+  getGenres(state);
+  doAutocomplete($("#search"));
   handleSearch($("#btn"), $("#search"));
-
+  getMovieData("Inception");
 });
-
-
-
-  // function makeQuery(baseUrl, callback, dataObj) {
-  //   let query = {
-  //     url: baseUrl,
-  //     type: "GET",
-  //     dataType: "json",
-  //     success: callback,
-  //     error: function() {
-  //       console.error("There is an error");
-  //     }
-  //   };
-  //   if (typeof dataObj !== "undefined") {
-  //     query.data = dataObj;
-  //   }
-  //   return query;
-  // }
-
-  // function getIDDoReq(data) {
-  //   let urlSearchID = "https://api.themoviedb.org/3/movie/"+data.results[0].id+API_KEY;
-  //   let q = makeQuery(urlSearchID, function() {
-  //
-  //   });
-  //   $.ajax(q);
-  // }
-
-  // function getTheMovieDB(searchTerm) {
-  //   let idQuery = makeQuery(S_BASE_URL, getIDDoReq, {query: searchTerm});
-  //   $.ajax(idQuery);
-  // }
-
-
-
-//getTheMovieDB("Inception")
